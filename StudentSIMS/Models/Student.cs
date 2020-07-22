6bf3b34fbc437d70d3d4e44ed27c6f3632af7210
joyhumin <joyhumin@gmail.com>
2020-07-22 17:25:20 +1200

    Build StudentSIMS api

---
+++ StudentSIMS/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentSIMS.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentId { get; set; }
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int PhoneNumber { get; set; }
+        //[Timestamp]
+        public DateTime TimeCreated { get; set; }
+
+        //public virtual ICollection<Address> Addresses { get; set; }
+    }
+}
